bbd063bfa13ded9f289ce1257f5433af3dc7e93f
Mona Lisa <din@ding.com>
2018-10-29 12:03:55 -0700

    d

--- temp remote repository/AppDelegate.swift
+++ temp remote repository/AppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var i : int!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
