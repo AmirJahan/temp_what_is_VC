f4975ea2699e547e92adaf9835e9a266a679e706
Amir J <Amir@amirMac.local>
2018-10-29 11:45:24 -0700

    User B changed some stuff

--- temp remote repository/ViewController.swift
+++ temp remote repository/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  temp remote repository
-//
-//  Created by Amir J on 10/29/18.
-//  Copyright © 2018 Cinard - How to Make an App. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
 
 }
