631b38bd7f2be9cb4304f0dea5b122e330d6876f
Amir J <Amir@amirmac.local>
2018-10-29 11:17:58 -0700

    Initial Commit

---
+++ temp remote repository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  temp remote repository
+//
+//  Created by Amir J on 10/29/18.
+//  Copyright © 2018 Cinard - How to Make an App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
